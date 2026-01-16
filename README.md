70481921f2a99dddb3e6bd0fe394c267a8e19892
adwad343 <advaitwadke2@gmail.com>
2026-01-16 20:37:17 +0530

    flex box added

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # scientific-calculator
 this a simple representation of scientific calci
 i think you are getting it
+this is our final year project
+
--- file1.js
+++
@@ -1,6 +0,0 @@
-let y = document.createElement("button");
-y.textContent = "here";
-let r = document.getElementsByTagName("div");
-let di = document.createElement("div");
-di.setAttribute
-document.r.appendChild(y);
