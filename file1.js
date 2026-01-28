8f7bb5ba1eb020307e2ae186e2547f7055086d8f
adwad343 <advaitwadke2@gmail.com>
2026-01-29 01:25:36 +0530

    added functionality for simple addition,subtraction, multiplication, division, using shunting yard algo

--- file1.js
+++ file1.js
@@ -15,17 +15,57 @@ map.set("-", 1);
 map.set("*", 2);
 map.set("/", 2);
 
+
 // make a function to add operators along with apropriate importance into the stack
-function add() {
+function add(x) {// adding operators according to shunting yard algo
+    if (operator.length == 0) {
+        operator.push(x);
+        return;
+    }
+
+    if (map.get(operator[operator.length - 1]) < map.get(x)) {
+        operator.push(x);
+        return;
+    }
+    while (map.get(operator[operator.length - 1]) <= map.get(x)) {
+        let curr = operator.pop();
+        numbers.push(curr);
+
+    }
+    operator.push(x);
+
+
+
 
 
 }
+
+function calculate(l, r, op) {
+    switch (op) {
+
+        case '+': return l + r;
+
+        case '-': return l - r;
+
+        case '*': return l * r;
+
+        case '/':
+            {
+                if (r == 0) {
+                    alert("cannot divide by zero");
+                }
+                return l / r;
+            }
+
+    }
+}
+
 
 
 let equals = document.getElementById("eq");
 equals.onclick = () => {
     let equation = input.value;
-    console.log(typeof (equation));
+    //console.log(typeof (equation));
     let n = 0;
     for (let i = 0; i < equation.length; i++) {
         let curr = equation[i];
@@ -33,26 +73,56 @@ equals.onclick = () => {
         if (map.has(curr)) {
             numbers.push(n);
             n = 0;
-
-
-
-
-
-
+            add(curr);
 
         }
         else {
-            n = n * 10 + curr;
+            n = n * 10 + Number(curr);
         }
 
+
     }
+    numbers.push(n);
+    numbers.push(...operator);
+    console.log(numbers);
+
+
+    numbers.forEach(i => {
+        console.log("i=", i, typeof (i));
+    });
+
+    // numbers is the shunt yard stack with correct operator precedence
+    // console.log(operator);
+    // console.log(numbers);
+    let stack = []; // storing final outptu
+    for (let i = 0; i < numbers.length; i++) {
+        if (map.has(numbers[i])) {
+            if (stack.length < 2) alert("enter valid input");
+            let r = stack.pop();
+            let l = stack.pop();
+
+            console.log("l=", typeof (l));
+            stack.push(calculate(l, r, numbers[i]));
+
+        }
+        else stack.push(numbers[i]);
+
+        // console.log(stack);
+    }
+
+
+    input.value = stack.pop();
+
+
+
+
 
 
 }
 
 
 
-const set = new Set("+-*/");
+
 
 let a = document.getElementsByTagName("button");
 for (let i = 0; i < a.length; i++) {
